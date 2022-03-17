f82ef798de6c17c126cea3c4527b5f3593d7026e
froliol <froliol>
2022-03-17 18:23:44 +0000

    test 3
    test 3

--- load-data.py
+++ load-data.py
@@ -3,4 +3,36 @@ df = spark.read.option("header", True).option("inferSchema", True).csv("file:/Wo
 
 # COMMAND ----------
 
-/Workspace/Repos/louis.frolio@databricks.com/Databricks-DW-Demo/data/products.csv
+url = 'https://raw.githubusercontent.com/froliol/Databricks-DW-Demo/2dafd082a3c1d250d9336d9aa7c5da98a92355bc/data/products.csv'
+from pyspark import SparkFiles
+spark.sparkContext.addFile(url)
+df = spark.read.csv(SparkFiles.get("products.csv"), header=True)
+
+# COMMAND ----------
+
+url = "https://github.com/froliol/Databricks-DW-Demo/blob/2dafd082a3c1d250d9336d9aa7c5da98a92355bc/data/?format=csv"
+     
+from pyspark import SparkFiles
+sc.addFile(url)
+     
+path  = SparkFiles.get('download')
+df = spark.read.csv("file://" + path, header=True, inferSchema= True, sep = ";")
+
+# COMMAND ----------
+
+data/products.csv
+https://github.com/froliol/Databricks-DW-Demo/blob/2dafd082a3c1d250d9336d9aa7c5da98a92355bc/data/products.csv
+https://raw.githubusercontent.com/froliol/Databricks-DW-Demo/2dafd082a3c1d250d9336d9aa7c5da98a92355bc/data/products.csv?token=GHSAT0AAAAAABSTU7MQI5ZXSKUJ23GQ2ZMWYRTPK5A
+
+# COMMAND ----------
+
+url = 'https://raw.githubusercontent.com/jokecamp/FootballData/master/openFootballData/cities.csv'
+from pyspark import SparkFiles
+spark.sparkContext.addFile(url)
+df = spark.read.csv(SparkFiles.get("cities.csv"), header=True)
+
+# COMMAND ----------
+
+import pandas as pd
+df = pd.read_csv("./data/products.csv")
+df
