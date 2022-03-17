dd9733cd7501f40b07e58ede57f27704f267ade6
froliol <froliol>
2022-03-17 20:36:15 +0000

    working
    download gz2, then copy to dbfs

--- load-data.py
+++ load-data.py
@@ -1,36 +1,10 @@
 # Databricks notebook source
 # MAGIC %sh
-# MAGIC wget -P /tmp https://github.com/froliol/Databricks-DW-Demo/blob/main/data/products.csv.zip
+# MAGIC wget -P /tmp https://github.com/froliol/Databricks-DW-Demo/blob/main/data/products.csv.bz2
 
 # COMMAND ----------
 
-# MAGIC %fs ls file:/tmp
-
-# COMMAND ----------
-
-# MAGIC %fs cp file:/tmp/sales_reps.csv.gz /tmp/sales_reps.csv.gz
-
-# COMMAND ----------
-
-# MAGIC %fs ls /tmp
-
-# COMMAND ----------
-
-# MAGIC %fs ls file:/tmp
-
-# COMMAND ----------
-
-#%fs cp file:/tmp/sales_rep.csv.gz dbfs:/tmp/sales_rep.csv.gz
-dbutils.fs.ls("file:/tmp/sales_reps.csv.gz")
-
-# COMMAND ----------
-
-df = spark.read.csv("file:/tmp/sales_rep.csv.gz")
-
-# COMMAND ----------
-
-#dbutils.fs.mv("file:/tmp/sales_rep.csv.gz","dbfs:/tmp/sales_rep.csv.gz")
-dbutils.fs.mv("dbfs:/tmp/sales_rep.csv.gz", "file:/tmp/sales_rep.csv.gz")
+# MAGIC %fs cp file:/tmp/products.csv.bz2 /tmp/products.csv.bz2
 
 # COMMAND ----------
 
@@ -38,12 +12,4 @@ dbutils.fs.mv("dbfs:/tmp/sales_rep.csv.gz", "file:/tmp/sales_rep.csv.gz")
 
 # COMMAND ----------
 
-# MAGIC %fs cp file:/tmp/sales_reps.csv.gz /tmp/sales_reps.csv.gz
-
-# COMMAND ----------
-
-dbutils.fs.ls("/tmp")
-
-# COMMAND ----------
-
-
+# MAGIC %fs ls /tmp/
