a454398aa29b67352ed18e5144bb2969e3d48053
froliol <froliol>
2022-03-17 20:20:02 +0000

    dd
    ddd

--- load-data.py
+++ load-data.py
@@ -1,19 +1,52 @@
 # Databricks notebook source
-import urllib 
-
-urllib.request.urlretrieve("https://github.com/froliol/Databricks-DW-Demo/raw/main/data/sales_reps.zip", "/tmp/sales_reps.csv.zip")
+url = "https://raw.githubusercontent.com/froliol/Databricks-DW-Demo/main/data/sales_reps.csv?token=GHSAT0AAAAAABSTU7MQYJ5THYZKTCLTIVNOYRTRMDQ"
+from pyspark import SparkFiles
+spark.sparkContext.addFile(url)
 
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC wget("https://github.com/froliol/Databricks-DW-Demo/blob/main/data/sales_reps.zip")
+# MAGIC ls data
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC wget -P /tmp https://github.com/froliol/Databricks-DW-Demo/blob/main/data/sales_reps.csv.gz
+
+# COMMAND ----------
+
+# MAGIC %fs ls /tmp
+
+# COMMAND ----------
+
+# MAGIC %fs ls file:/tmp
+
+# COMMAND ----------
+
+#%fs cp file:/tmp/sales_rep.csv.gz dbfs:/tmp/sales_rep.csv.gz
+dbutils.fs.ls("file:/tmp/sales_reps.csv.gz")
+
+# COMMAND ----------
+
+df = spark.read.csv("file:/tmp/sales_rep.csv.gz")
+
+# COMMAND ----------
+
+#dbutils.fs.mv("file:/tmp/sales_rep.csv.gz","dbfs:/tmp/sales_rep.csv.gz")
+dbutils.fs.mv("dbfs:/tmp/sales_rep.csv.gz", "file:/tmp/sales_rep.csv.gz")
+
+# COMMAND ----------
+
+# MAGIC %fs ls file:/tmp/
+
+# COMMAND ----------
+
+# MAGIC %fs cp file:/tmp/sales_reps.csv.gz /tmp/sales_reps.csv.gz
+
+# COMMAND ----------
+
+dbutils.fs.ls("/tmp")
 
 # COMMAND ----------
 
 
-
-# COMMAND ----------
-
-import pandas as pd
-df = pd.read_csv("./data/products.csv.zip")
-df
